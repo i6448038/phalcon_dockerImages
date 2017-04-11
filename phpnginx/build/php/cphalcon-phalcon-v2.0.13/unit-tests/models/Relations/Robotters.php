18a6b4475486249a987e5ed03fb899e9a8cec110
i6448038 <376832293@qq.com>
2017-04-11 23:54:03 +0800

    php5.6 phalcon2.0 nginx

---
+++ phpnginx/build/php/cphalcon-phalcon-v2.0.13/unit-tests/models/Relations/Robotters.php
@@ -0,0 +1,36 @@
+<?php
+
+use Phalcon\Mvc\Model;
+
+/**
+ * Robotters
+ *
+ * "robotters" is robots in danish
+ */
+class Robotters extends Model
+{
+
+	public function getSource()
+	{
+		return 'robots';
+	}
+
+	public function columnMap()
+	{
+		return array(
+			'id' => 'code',
+			'name' => 'theName',
+			'type' => 'theType',
+			'year' => 'theYear',
+			'datetime' => 'theDatetime',
+			'text' => 'theText',
+		);
+	}
+
+	public function initialize()
+	{
+		$this->hasMany('code', 'RobottersDeles', 'robottersCode', array(
+			'foreignKey' => true
+		));
+	}
+}
